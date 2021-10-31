bedf50a101903ac0a9a7a744dbb5f7c8ac10b283
KimTaeyoun <kimty1121@gmail.com>
2021-10-31 23:49:48 +0900

    add comments Kruskal

--- Kruskal/KruskalPath.java
+++ Kruskal/KruskalPath.java
@@ -9,27 +9,28 @@ public class KruskalPath {
     
     public String find(String node) {
         // path compresion 기법
-        if (this.parent.get(node) != node) {
-            this.parent.put(node, this.find(this.parent.get(node)));
+        if (this.parent.get(node) != node) { // 현재 parent에 있는 노드가 루트 노드가 아니면
+            this.parent.put(node, this.find(this.parent.get(node))); //부모노드를 전부 체크해서 결과적으로는 루트노드로 연결을 하게한 후에
         }
-        return this.parent.get(node);
+        return this.parent.get(node); //결과적으로 해당 셋에 루트노드를 리턴하게 함
     }
     
     public void union(String nodeV, String nodeU) {
-        String root1 = this.find(nodeV);
-        String root2 = this.find(nodeU);
+        String root1 = this.find(nodeV); // nodeV의 루트 노드를 가져온다
+        String root2 = this.find(nodeU); // nodeU의 루트 노드를 가져온다
         
         // union-by-rank 기법
-        if (this.rank.get(root1) > this.rank.get(root2)) {
-            this.parent.put(root2, root1);
+        if (this.rank.get(root1) > this.rank.get(root2)) { //root1의 랭크가 더 높다면,
+            this.parent.put(root2, root1); //root2의 부모노드를 root1로 세팅한다
         } else {
             this.parent.put(root1, root2);
-            if (this.rank.get(root1) == this.rank.get(root2)) {
+            if (this.rank.get(root1) == this.rank.get(root2)) { //랭크가 동일하면 root2의 랭크를 1더 높힌다
                 this.rank.put(root2, this.rank.get(root2) + 1);
             }
         }
     }
     
+    // 초기화: 전체 노드를 하나씩 받아서 자신을 루트 노드로 만들고 랭크를 0으로 만든다.
     public void makeSet(String node) {
         this.parent.put(node, node);
         this.rank.put(node, 0);
@@ -48,7 +49,7 @@ public class KruskalPath {
         Collections.sort(edges);
         
         for (int index = 0; index < edges.size(); index++) {
-            currentEdge = edges.get(index);
+            currentEdge = edges.get(index); //탐욕 알고리즘 기반으로 소트한 후에
             if (this.find(currentEdge.nodeV) != this.find(currentEdge.nodeU)) {
                 this.union(currentEdge.nodeV, currentEdge.nodeU);
                 mst.add(currentEdge);
