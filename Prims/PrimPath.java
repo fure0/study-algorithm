dceda165c60ee06e04036f895982aa7ec2415018
KimTaeyoun <kimty1121@gmail.com>
2021-11-02 22:33:52 +0900

    studying prims path

---
+++ Prims/PrimPath.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class PrimPath {
+ 
+    public static void main(String[] args) {
+        ArrayList<Edge> myedges = new ArrayList<Edge>();
+        myedges.add(new Edge(7, "A", "B"));
+        myedges.add(new Edge(5, "A", "D"));
+        myedges.add(new Edge(8, "B", "C"));
+        myedges.add(new Edge(9, "B", "D"));
+        myedges.add(new Edge(7, "D", "E"));
+        myedges.add(new Edge(5, "C", "E"));
+        myedges.add(new Edge(7, "B", "E"));
+        myedges.add(new Edge(6, "D", "F"));
+        myedges.add(new Edge(8, "E", "F"));
+        myedges.add(new Edge(9, "E", "G"));
+        myedges.add(new Edge(11, "F", "G"));
+
+        System.out.println(myedges);
+    }
+}
