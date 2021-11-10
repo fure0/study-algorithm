83bfd35c62d6cb96bb47067143a8f530931b49d2
KimTaeyoun <kimty1121@gmail.com>
2021-11-10 23:49:03 +0900

    add comments NQueen

--- BackTracking/NQueen.java
+++ BackTracking/NQueen.java
@@ -13,16 +13,16 @@ public class NQueen {
     }
     
     public void dfsFunc(Integer N, Integer currentRow, ArrayList<Integer> currentCandidate) {
-        if (currentRow == N) {
+        if (currentRow == N) { // 여기까지 온거면 전체 다 확인한거라 더 진행 할 필요가 없다
             System.out.println(currentCandidate);
             return ;
         }
         
-        for (int index = 0; index < N; index++) {
-            if (this.isAvailable(currentCandidate, index) == true) {
+        for (int index = 0; index < N; index++) { // 0 ~ 4 까지 
+            if (this.isAvailable(currentCandidate, index) == true) { // 다른 퀸의 이동 범위에 침범하지 않으면
                 currentCandidate.add(index);
-                this.dfsFunc(N, currentRow + 1, currentCandidate);
-                currentCandidate.remove(currentCandidate.size() - 1);
+                this.dfsFunc(N, currentRow + 1, currentCandidate); //다음열 확인 하기위해 +1
+                currentCandidate.remove(currentCandidate.size() - 1); //여기를 통과한다면 후보군이 없기 때문에 해당 행 자체를 가지치기한다.
             }
         }
     }   
@@ -30,7 +30,7 @@ public class NQueen {
     public static void main(String[] args) {
 
         NQueen nObject = new NQueen();
-        nObject.dfsFunc(4, 0, new ArrayList<Integer>());
+        nObject.dfsFunc(4, 0, new ArrayList<Integer>()); // 4x4의 크기, 0열 부터 시작(행은 볼 필요가 없음), 처음엔 빈 리스트 입력
 
     }
 }
